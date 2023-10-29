2c86f76e008bdfd68eec9b368b93d26a6f4ea041
Fabrizio Nicosia <fabrizio.nic@gmail.com>
2023-10-29 16:23:13 +0100

    creata classe Redirect

--- app/Core/Http/Redirect.php
+++ app/Core/Http/Redirect.php
@@ -1,16 +1,23 @@
 <?php
 
 namespace Core\Http;
+use Core\Http\enum\HttpstatusCode;
 
 class Redirect extends Response
 {
     public function __construct(string $url)
     {
+        parent::__construct('', HttpstatusCode::Redirect, ['location' => $url] );
 
     }
 
     public function conMessaggio()
     {
         
+    }
+
+    public function invia()
+    {
+        return header('Location'.$this->header['location'], true, $this->codice->value);
     }
 }
