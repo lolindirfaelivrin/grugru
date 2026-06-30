97f56594d3a9112cf4e23c5e86b00d697cc89f0a
Fabrizio Nicosia <fabrizio.nic@gmail.com>
2026-06-30 14:43:10 +0200

    Creata classe che gestisce il numero di versione di GruGru

--- app/Core/Versione.php
+++ app/Core/Versione.php
@@ -4,12 +4,25 @@ namespace Core;
 
 final class Versione
 {
-    public const VERSION = '0.12.3';
-    public const MAJOR = 0;
-    public const MINOR = 12;
-    public const PATCH = 1;
-    public const EXTRA = '';  // 'alpha', 'beta', 'rc1', ecc.
-    public static function versione(): string
+    public const string VERSION = '0.12.3';
+    public const string EXTRA = 'beta';
+
+    public static function major(): int
+    {
+        return (int) explode('.', self::VERSION)[0];
+    }
+
+    public static function minor(): int
+    {
+        return (int) explode('.', self::VERSION)[1];
+    }
+
+    public static function patch(): int
+    {
+        return (int) explode('.', self::VERSION)[2];
+    }
+
+    public static function full(): string
     {
         $extra = self::EXTRA ? '-' . self::EXTRA : '';
         return self::VERSION . $extra;
