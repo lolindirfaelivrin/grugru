01bf2833e2c83e976ce58d5d565f2aea97cec477
Fabrizio Nicosia <fabrizio.nic@gmail.com>
2026-07-13 11:08:09 +0200

    Trasformata in classe di tipo final

--- app/Libreria/Autoload.php
+++ app/Libreria/Autoload.php
@@ -1,25 +1,20 @@
 <?php
-declare(strict_types=1); 
+declare(strict_types=1);
 namespace Libreria;
-use Core\GruGru;
 
-/**
- * Simple autoloader, so we don't need Composer just for this.
- * https://www.php.net/manual/en/language.oop5.autoload.php
- */
-class Autoload
+final class Autoload
 {
 	public static function register()
 	{
 		spl_autoload_register(function ($class) {
 			$base = dirname(__DIR__);
-			$file = str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php';
+			$file = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
 
-			if (file_exists($base.DIRECTORY_SEPARATOR.$file)) {
-				require_once $base.DIRECTORY_SEPARATOR.$file;
+			if (file_exists($base . DIRECTORY_SEPARATOR . $file)) {
+				require_once $base . DIRECTORY_SEPARATOR . $file;
 				return true;
 			}
-            return false;
+			return false;
 		});
 	}
 }
