4c3ae737dfb84081d03c9af702bd7441286a8e2d
Fabrizio Nicosia <fabrizio.nic@gmail.com>
2026-07-19 18:07:10 +0200

    Aggiunte classi in caricamento

--- faber.php
+++ faber.php
@@ -1,7 +1,10 @@
 #!/usr/bin/php
 <?php
 
+use Core\Faber\Comandi\ListaComandi;
 use Libreria\Autoload;
+
+
 
 if (php_sapi_name() !== 'cli') {
     exit;
@@ -13,4 +16,15 @@ require 'app/Libreria/Autoload.php';
 Autoload::register();
 
 $faber = new \Core\Faber\Faber();
-$faber->eseguiComando($argv);
+$faber->regista(new ListaComandi($faber));
+
+
+$faber->registraComandi([
+    new \Core\Faber\Comandi\InfoComandi(),
+    new \Core\Faber\Comandi\ManutenzioneComando(),
+    new \Core\Faber\Comandi\NuovoComandoComandi(),
+    new \Core\Faber\Comandi\PhpServerComandi(),
+]);
+
+$codice_uscita = $faber->esegui($argv);
+exit($codice_uscita);
